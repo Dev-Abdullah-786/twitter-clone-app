f16b7d1982bfa07a23f4daa76f66a58421e1cfe0
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-02-23 17:34:56 +0500

    update Tailwind config with custom Twitter color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitterWhite: "#e7e9ea",
+        twitterBlue: "#308CD8",
+        twitterBorder: "#2f3336",
+        twitterLightGray: "#71767b",
+        twitterDarkGray: "#17181C",
+      },
+    },
   },
   plugins: [],
 };
