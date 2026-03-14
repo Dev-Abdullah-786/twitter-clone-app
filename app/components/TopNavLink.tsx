820edda75bbceeb00ad5be5c567f7e27d5af61ec
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-03-14 11:23:23 +0500

    create reusable TopNavLink component with icon and link support

---
+++ app/components/TopNavLink.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+interface TopNavLinkProps {
+  title?: string;
+  url?: string;
+}
+
+export default function TopNavLink({
+  title = "Tweet",
+  url = "/",
+}: TopNavLinkProps) {
+  return (
+    <Link href={url}>
+      <div className="flex mb-2 cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 mr-3"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+          />
+        </svg>
+        {title}
+      </div>
+    </Link>
+  );
+}QD
