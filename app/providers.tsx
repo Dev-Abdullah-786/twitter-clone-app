0e9f67cd963cc5a42e941e97f77c50060ef66a59
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-03-24 08:46:55 +0500

    add javascript-time-ago localization to Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import TimeAgo from "javascript-time-ago";
 import { SessionProvider } from "next-auth/react";
+import en from "javascript-time-ago/locale/en.json";
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+TimeAgo.addDefaultLocale(en);
+
+export default function Providers({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
